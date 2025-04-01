26314a641cea3874de4a6a96dbc1a2410103aedf
BopReznole <bopreznole@protonmail.com>
2025-04-02 01:19:56 +0300

    Update routine_steps_h.c

--- lab01/routine_steps_h.c
+++ lab01/routine_steps_h.c
@@ -1,9 +1,9 @@
-int fibonacci(int n)
+int fibonacci(int n)	//n<=9
 {
-  if (n == 0)
-  return 0;
-  else if (n == 1)
-  return 1;
-  else
-  return fibonacci(n-1) + fibonacci(n-2);
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else
+		return fibonacci(n-1) + fibonacci(n-2);
 }
