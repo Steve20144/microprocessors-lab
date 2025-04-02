b9578da348e80479d8c6d030afa0bb104391c0b7
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2025-04-02 19:10:58 +0300

    Working routine_steps_a-e.s

--- lab01/routine_steps_a-e.s
+++ lab01/routine_steps_a-e.s
@@ -35,7 +35,7 @@ lower_case:
 	blt check_numbers		//If lower than 'a' proceed to check_numbers
 	cmp r2, #'z'			//Compare current character with 'z'
 	bgt check_numbers		//If gretaer than 'z' proceed to check_numbers
-	rsb r6, r6, #97			//r6 = 97 - ASCII
+	rsb r6, r2, #97			//r6 = 97 - ASCII
 	mul r6, r6, r6			//Square the result
 	add r4, r4, r6			//Add to lowercase additive (tbn accumulator)
 	b loop
@@ -45,11 +45,21 @@ check_numbers:
 	blt loop				//If lower than '0' branch to loop
 	cmp r2, #'9'			//Compare current character with '9'
 	bgt loop				//If greater than '9' branch to loop
-	ldr r8, =values			//Load base address of the array
-	ldr r8, [r8, r2, lsl #2]	//Load values[r2] into r8. lsl because one word is 4 bytes so we need to offset by that.
+	sub r8, r2, #'0'		//Convert ASCII to digit
+	ldr r6, =values			//Load base address of the array
+	ldr r8, [r6, r8, lsl #2]	//Load values[r2] into r8. lsl because one word is 4 bytes so we need to offset by that.
 	add r7, r7, r8			//Add the number to the numbers accumulator
 	b loop
+
+end:
+	add r1, r1, r3			//Add the capital letters counter
+	add r1, r1, r4			//Add the lowercase letters counter
+	add r1, r1, r7			//Add the numbers accumulator
+	mov r0, r1	
+	pop {r4, r5, r6, r7, r8, lr}
+	bx lr
 	
+		
 
 	
 	.fnend
