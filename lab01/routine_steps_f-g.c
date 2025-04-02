9e05caed0a5ef185d8923c7d7824822f21a0588f
BopReznole <bopreznole@protonmail.com>
2025-04-02 11:07:17 +0300

    Update routine_steps_f-g.c

--- lab01/routine_steps_f-g.c
+++ lab01/routine_steps_f-g.c
@@ -14,3 +14,27 @@ int sum_mod7(int hash){
 		return hash;	//0<=hash<=9
 	}
 }
+
+
+//KEIL DISSASEMBLER - self notes
+// #include <stdio.h>
+// #include <uart.h>
+
+// void factorial (int x){
+// 	int y=0;
+// 	y=x % 7;
+// };
+//      6:         y=x % 7; 
+// //0x080007B4 9801      LDR           r0,[sp,#0x04]
+// //0x080007B6 F2424193  MOVW          r1,#0x2493
+// //0x080007BA F2C92149  MOVT          r1,#0x9249
+// //0x080007BE FB510200  SMMLA         r2,r1,r0,r0
+// //0x080007C2 1091      ASRS          r1,r2,#2
+// //0x080007C4 EB0171D2  ADD           r1,r1,r2,LSR #31
+// //0x080007C8 EBC101C1  RSB           r1,r1,r1,LSL #3
+// //0x080007CC 1A40      SUBS          r0,r0,r1
+// //0x080007CE 9000      STR           r0,[sp,#0x00]
+// int main(){
+// 	factorial(10);
+// 	return 0;
+// }
