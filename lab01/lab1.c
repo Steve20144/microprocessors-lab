9fac9e0823ff067011911b27097b1d83afcf58f8
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2025-04-03 14:17:04 +0300

    Update lab1.c

--- lab01/lab1.c
+++ lab01/lab1.c
@@ -5,14 +5,18 @@ extern int hashthestring(const char *str);
 extern int addandmod(const int answer);
 extern uint32_t hashresult;
 extern int fibonacci(const int previousresult);
+extern int xor(const char *str);
+extern uint32_t xor_checksum;
 
 int main() {
     const char *test_str = "A9b3";
     hashthestring("A9b3");
 		int result2 = addandmod(hashresult);
 		int result3 = fibonacci(result2);
+		xor(test_str);
     printf("Hash value for '%s': %d\n", test_str, hashresult);
 		printf("Sum and mod for '%s': %d\n", test_str, result2);
 		printf("Fibonacci result for '%s': %d\n", test_str, result3);
+		printf("XOR Checksum result for '%s': %d\n", test_str, xor_checksum);
     return 0;
 }
