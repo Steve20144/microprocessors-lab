877e9c97253087c51c547768fcb5cad7aba17e68
BopReznole <bopreznole@protonmail.com>
2025-05-28 00:08:20 +0300

    Create dht11.h

---
+++ lab03/dht11.h
@@ -0,0 +1,22 @@
+/*!
+ * \file      dht11.h
+ * \brief     API for reading temperature and humidity from a DHT11 sensor.
+ * \copyright ARM University Program &copy; ARM Ltd 2014.
+ */
+#ifndef DHT11_H
+#define DHT11_H
+
+#include <stdint.h>
+
+/*! \brief Reads 5 bytes of data from a DHT11 sensor.
+ *  \param[out] data_out  Pointer to a 5-byte buffer:
+ *                        [0] humidity integer part,
+ *                        [1] humidity decimal (always 0 for DHT11),
+ *                        [2] temperature integer part,
+ *                        [3] temperature decimal (always 0 for DHT11),
+ *                        [4] checksum.
+ *  \return 0 on success (checksum valid), -1 on failure.
+ */
+int dht11_poll(uint8_t* data_out);
+
+#endif // DHT11_H
