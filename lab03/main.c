f2a3f5641063631878704afe894035c686aac5a2
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2025-05-29 13:32:19 +0300

    Update main.c

--- lab03/main.c
+++ lab03/main.c
@@ -10,6 +10,7 @@
 #include "stm32f4xx.h"    // CMSIS/LL definitions
 #include "platform.h"     // Board-specific base addresses, IRQs, pin-mux macros
 #include <stdio.h>        // sprintf, printf
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
@@ -23,11 +24,12 @@
 #define BAUD_RATE             115200
 #define UART_RX_BUFFER_SIZE   128
 #define MAX_DIGITS            128
-#define LED_PIN								PA_2
+#define LED_PIN               PA_2
+#define PINCODE 							123
 
 volatile int counter_button = 0;
 volatile char buff2[64];
-typedef enum { mode_a, mode_b} mode_profile;
+typedef enum { mode_a, mode_b } mode_profile;
 volatile mode_profile profile = mode_a;
 volatile int refresh_rate = 1;
 volatile int aem = 12345;
@@ -42,216 +44,281 @@ uint32_t buff_index;
 Queue rx_queue;
 
 // Function prototypes
-void button_isr(int sources);	// for the touch sensor interrupt
-void uart_rx_isr(uint8_t rx);	// for the uart
-void timer_isr(void);					// for the timer interrupt
-void status_report(void);			// for the status report
-void dht_print(void);					// prints stuff from dht
-void alert_mode(void);				// actions when mode_b is on
+void button_isr(int sources);    // for the touch sensor interrupt
+void uart_rx_isr(uint8_t rx);    // for the uart
+void timer_isr(void);            // for the timer interrupt
+void status_report(void);        // for the status report
+void dht_print(void);            // prints stuff from dht
+void alert_mode(void);           // actions when mode_b is on
 
-int main(void){
-	uint8_t rx_char = 0;
-	//static uint8_t* results;
-	char buff[64];
-	
-	// --- Initialize GPIO touch-sensor ---
-	gpio_set_mode(PA_1, PullDown);           // on-board switch as pull-down
-	gpio_set_trigger(PA_1, Rising);          // trigger on rising edge
-	gpio_set_callback(PA_1, button_isr);
-	gpio_set_mode(PA_1, Input);		//it works this way, maybe I should remove the Pulldown mode
-	
-	// --- Initialize UART with RX queue ---
-	queue_init(&rx_queue, UART_RX_BUFFER_SIZE);
-	uart_init(BAUD_RATE);
-	uart_set_rx_callback(uart_rx_isr);
-	uart_enable();
+static void sample_dht11(void);
+static void board_init(void);
+static void application_loop(void);
+static void system_login(void);
 
-	// --- Initialize LED ---
-	//DOESNT WORK IF I ENABLE THESE
-	//gpio_set_mode(LED_PIN, Output);
-	//gpio_set(LED_PIN, 0);
-	
-	// --- Initialize DHT11 ---
-	gpio_set_mode(PA_0, Output);
-	gpio_set(PA_0, 1);
-	sprintf(buff,"gpio: %d\r\n", gpio_get(PA_0));
-	uart_print(buff);
-	
-	// --- Initialize 0.1s timer (100,000 µs) ---
-	//(CLK_FREQ/10)=1s
-	timer_init(CLK_FREQ/10);		//clock_freq=160khz??
-	timer_disable();				// start disabled
-	timer_set_callback(timer_isr);
-	
-	// --- Set interrupt priorities ---
+int main(void) {
+    board_init();          // one-time setup
+		system_login();					//Password and AEM logic
+    application_loop();    // runs until you want to exit
+    NVIC_SystemReset();    // reset if loop ever returns
+    while (1) { /* should never get here */ }
+}
 
-	NVIC_SetPriorityGrouping(2);
-
-	NVIC_SetPriority(TIM2_IRQn,			2);	// Timer priority
-	NVIC_SetPriority(USART2_IRQn,		1);	// UART priority
-	NVIC_SetPriority(EXTI15_10_IRQn,	3);	// Button highest
-	__enable_irq();
-	
-	
-	//DHT11 test
-	results = dht11_poll();
-	sprintf(buff, "Humidity: %d.%d%%RH\r\nTemperature: %d.%dC\r\n", 
-		results[0], results[1], results[2], results[3]);
-	uart_print(buff);
-	free(results);
-	
-	//touch-sensor test
-	while(0){	//turned off for now
-		//sprintf(buff, "Touch sensor: %d\r\n", gpio_get(PA_1));
-		//uart_print(buff);
-		delay_ms(1000);
-		//break;
+// ------------------------------------------------------------------------------------------------
+//DHT11 Sample and print
+static void sample_dht11(void){
+	// --- One-shot DHT11 test read ---
+    results = dht11_poll();
+    {
+        char msg[64];
+        sprintf(msg,
+                "Humidity: %d.%d%%RH\r\nTemperature: %d.%dC\r\n",
+                results[0], results[1],
+                results[2], results[3]);
+        uart_print(msg);
+    }
+    free(results);
 	}
-	
-	// UART input #START
-	uart_print("\r\nEnter a number: ");
 
-	// --- Main loop: prompt, read line, then start blinking ---
-	while (1) {
+
+// Initialize GPIO, UART, timer, DHT11, NVIC, etc.
+static void board_init(void) {
+    // --- Initialize GPIO touch-sensor on PA1 ---
+    gpio_set_mode(PA_1, PullDown);
+    gpio_set_trigger(PA_1, Rising);
+    gpio_set_callback(PA_1, button_isr);
+    gpio_set_mode(PA_1, Input);
+
+    // --- Initialize UART with RX queue ---
+    queue_init(&rx_queue, UART_RX_BUFFER_SIZE);
+    uart_init(BAUD_RATE);
+    uart_set_rx_callback(uart_rx_isr);
+    uart_enable();
+
+    // --- (LED_PIN setup, disabled if not working) ---
+    // gpio_set_mode(LED_PIN, Output);
+    // gpio_set(LED_PIN, 0);
+
+    // --- Initialize DHT11 pin and debug print ---
+    gpio_set_mode(PA_0, Output);
+    gpio_set(PA_0, 1);
+    {
+        char tmp[64];
+        sprintf(tmp, "gpio PA0: %d\r\n", gpio_get(PA_0));
+        uart_print(tmp);
+    }
+
+    // --- Initialize 0.1s timer ---
+    timer_init(CLK_FREQ/10);
+    timer_disable();
+    timer_set_callback(timer_isr);
+
+    // --- Set interrupt priorities and enable IRQs ---
+    NVIC_SetPriorityGrouping(2);
+    NVIC_SetPriority(TIM2_IRQn,       2);
+    NVIC_SetPriority(USART2_IRQn,     1);
+    NVIC_SetPriority(EXTI15_10_IRQn,  3);
+    __enable_irq();
+
+    
+
+    
+}
+
+// ------------------------------------------------------------------------------------------------
+//Login Function: Enter password and AEM
+
+static void system_login(void) {
+		//Disable the button interrupt while we're in the login loop
+		NVIC_DisableIRQ(EXTI0_IRQn);
+    uint8_t rx_char = 0;
+	uint32_t idx =0;
+	
+		//PIN ENTRY
+		while (1) {
+        uart_print("Enter PIN: ");
+        idx = 0;
+
+        // collect characters until CR or full
+        do {
+            while (!queue_dequeue(&rx_queue, &rx_char)) {
+                __WFI();
+            }
+            if (rx_char == 0x7F && idx > 0) {
+                // backspace
+                idx--;
+                uart_tx(rx_char);
+            } else if (rx_char != 0x7F) {
+                buff[idx++] = (char)rx_char;
+                uart_tx(rx_char);
+            }
+        } while (rx_char != '\r' && idx < UART_RX_BUFFER_SIZE);
+
+        // overwrite the '\r' with '\0'
+        if (idx > 0) {
+            buff[idx - 1] = '\0';
+        }
+        uart_print("\r\n");
+
+        // overflow?
+        if (idx >= UART_RX_BUFFER_SIZE) {
+            uart_print("Stop trying to overflow my buffer! I resent that!\r\n");
+            continue;
+        }
+
+        // simple numeric compare
+        if (atoi(buff) == PINCODE) {
+            NVIC_EnableIRQ(EXTI0_IRQn);
+            break;
+        }
+        uart_print("Error: Wrong Passcode\r\n");
+    }
+
 		
-		//uart_print("Enter a number:");
-		buff_index = 0;
-		// Read chars into buff until '\r' or full
-		do {
+		 // At this point, PIN was correct and you've broken out
+    uart_print("PIN accepted. Unlocking...\r\n");
+				
 			
-			// Wait for next byte in queue
-			while (!queue_dequeue(&rx_queue, &rx_char)) {
-				__WFI();
-			}
-
-			if (rx_char == 0x7F) {  // backspace
-				if (buff_index > 0) {
-					buff_index--;
-					uart_tx(rx_char);  // echo backspace
-				}
-			} else {
-				buff[buff_index++] = (char)rx_char;
-				uart_tx(rx_char);    // echo
-			}
-		} while (rx_char != '\r' && buff_index < UART_RX_BUFFER_SIZE);
-
-		// Null-terminate string (overwrite '\r')
-		if (buff_index > 0) {
-			buff[buff_index - 1] = '\0';
-		}
-		uart_print("\r\n");
-
-		// Check for overflow
-		if (buff_index >= UART_RX_BUFFER_SIZE) {
-			uart_print("Stop trying to overflow my buffer! I resent that!\r\n");
-			continue;
-		}
-	// UART input #END
-
-	timer_enable();	//start timer isr
-	}
-	//timer_enable();	//start timer isr
-}
-
-// Button press ISR: toggle lock state on each press
-void button_isr(int sources) {
-	gpio_set(P_DBG_ISR, 1);
-	if ((sources << GET_PIN_INDEX(PA_1)) & (1 << GET_PIN_INDEX(PA_1))) {
-		switch (counter_button) {
-			//switch to mode B
-			case 0:
-				uart_print("switch to mode B\r\n");
-				break;
-			//switch to mode A
-			case 1:
-				uart_print("switch to mode A\r\n");
-				break;
-			//calculate new refresh rate
-			case 2:
-				uart_print("calculate new refresh rate\r\n");
-				break;
-		}
-		if (counter_button == 3) {
-			counter_button = 0;
-		}
-		else {
-			counter_button++;	//increment button counter
-		}
-	}
-	gpio_set(P_DBG_ISR, 0);
-}
-
-void timer_isr(void) {
-	//todo
-}
-
-void dht_print(void) {
-	results = dht11_poll();
-	sprintf(buff, "Humidity: %d.%d%%RH\r\nTemperature: %d.%dC\r\n", 
-		results[0], results[1], results[2], results[3]);
-	uart_print(buff);
-	free(results);
-}
-
-void alert_mode(void) {
-	if (profile==mode_b) {
-		//Temperature check
-		if (results[2]>25) {
-			temperature_cnt=5;
-		}
-		else {
-			if (temperature_cnt>0) {
-				temperature_cnt--;
-			}
-			else {
-				temperature_cnt=0;
-			}
-		}
-		//Humidity check
-		if (results[0]>60) {
-			humidity_cnt=5;
-		}
-		else {
-			if (humidity_cnt>0) {
-				humidity_cnt--;
-			}
-			else {
-				humidity_cnt=0;
-			}
-		}
-		//Blink led if temperature or humidity out of limits
-		if ( (temperature_cnt>0) || (humidity_cnt>0) ) {
-			gpio_toggle(LED_PIN);	//might not turn it off the last time
-		}
-		else if ( (temperature_cnt==0) && (humidity_cnt==0) ) {
-			gpio_set(LED_PIN, 0);	//turns it off the last time
-		}
-	}
-}
-
-void status_report(void) {
-	int profile_switches=0;
-	profile_switches = ( ((counter_button/3) * 2) + (counter_button%3) );
-	if (status_called==1) {
-		//print mode
-		if (profile==mode_a) {
-			uart_print("Mode A/r/n");
-		}
-		else {
-			uart_print("Mode B/r/n");
-		}
-		//print dht
-		//dht_print();
-		//profile switches
+			
 		
-		sprintf(buff2, "Profile switches: %d", profile_switches);
-		uart_print(buff2);
-	}
+	
 }
 
-// Push valid chars into rx_queue
+
+
+// Main loop: read UART line, then enable timer for periodic work
+static void application_loop(void) {
+    while (1) {
+        // --- Present menu ---
+        uart_print("\r\n=== Main Menu ===\r\n");
+        uart_print("a) Increase Sampling Rate by 1s\r\n");
+        uart_print("b) Decrease Sampling Rate by 1s\r\n");
+        uart_print("c) Change View\r\n");
+        uart_print("d) Print latest sample\r\n");
+        uart_print("Enter choice: ");
+
+        // Read one line of input into buff
+        buff_index = 0;
+        uint8_t rx_char;
+        do {
+            // Wait for next received char
+            while (!queue_dequeue(&rx_queue, &rx_char)) {
+                __WFI();
+            }
+            // Echo and handle backspace
+            if (rx_char == 0x7F && buff_index > 0) {
+                buff_index--;
+                uart_tx(rx_char);
+            } else if (buff_index < UART_RX_BUFFER_SIZE - 1) {
+                buff[buff_index++] = (char)rx_char;
+                uart_tx(rx_char);
+            }
+        } while (rx_char != '\r');
+
+        // Null-terminate (overwrite '\r')
+        if (buff_index > 0) {
+            buff[buff_index - 1] = '\0';
+        }
+        uart_print("\r\n");
+
+        // Dispatch choice
+        switch (buff[0]) {
+            case 'a':
+            case 'A':
+                //option_a();
+                break;
+            case 'b':
+            case 'B':
+                //option_b();
+                break;
+            case 'c':
+            case 'C':
+                //option_c();
+                break;
+            case 'd':
+            case 'D':
+                sample_dht11();
+                break;
+            default:
+                uart_print("Invalid option, please enter a, b, c, or d.\r\n");
+                continue;
+        }
+
+        // After handling, optionally start timer-driven work
+        timer_enable();
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// Button press ISR: toggle modes and report
+void button_isr(int sources) {
+    gpio_set(P_DBG_ISR, 1);
+    if ((sources << GET_PIN_INDEX(PA_1)) & (1 << GET_PIN_INDEX(PA_1))) {
+        switch (counter_button) {
+            case 0: uart_print("switch to mode B\r\n"); break;
+            case 1: uart_print("switch to mode A\r\n"); break;
+            case 2: uart_print("calculate new refresh rate\r\n"); break;
+        }
+        counter_button = (counter_button + 1) % 4;
+    }
+    gpio_set(P_DBG_ISR, 0);
+}
+
+// Periodic timer ISR
+void timer_isr(void) {
+    // TODO: periodic activity (e.g., call dht_print, alert_mode)
+}
+
+// Read and print DHT11 data
+void dht_print(void) {
+    results = dht11_poll();
+    sprintf(buff, "Humidity: %d.%d%%RH\r\nTemperature: %d.%dC\r\n",
+            results[0], results[1], results[2], results[3]);
+    uart_print(buff);
+    free(results);
+}
+
+// If in mode_b, check thresholds and blink LED_PIN
+void alert_mode(void) {
+    if (profile == mode_b) {
+        // Temperature check
+        if (results[2] > 25) {
+            temperature_cnt = 5;
+        } else if (temperature_cnt > 0) {
+            temperature_cnt--;
+        }
+
+        // Humidity check
+        if (results[0] > 60) {
+            humidity_cnt = 5;
+        } else if (humidity_cnt > 0) {
+            humidity_cnt--;
+        }
+
+        // Blink or turn off LED
+        if (temperature_cnt > 0 || humidity_cnt > 0) {
+            gpio_toggle(LED_PIN);
+        } else {
+            gpio_set(LED_PIN, 0);
+        }
+    }
+}
+
+// Print current mode and counters if requested
+void status_report(void) {
+    int profile_switches = ((counter_button / 3) * 2) + (counter_button % 3);
+    if (status_called) {
+        if (profile == mode_a) {
+            uart_print("Mode A\r\n");
+        } else {
+            uart_print("Mode B\r\n");
+        }
+        sprintf(buff2, "Profile switches: %d\r\n", profile_switches);
+        uart_print(buff2);
+    }
+}
+
+// UART RX ISR: enqueue all received bytes
 void uart_rx_isr(uint8_t rx) {
-	if ((rx >= '0' && rx <= '9') || rx == '\r' || rx == '\n' || rx == '-' || 1) {	//PASS ALL
-		queue_enqueue(&rx_queue, rx);
-	}
+    queue_enqueue(&rx_queue, rx);
 }
